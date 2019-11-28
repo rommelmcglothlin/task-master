436080553179fcbbcdb4fc49c2c951483459cc2c
rommelmcglothlin <rommel.mcglothlin@icloud.com>
2019-11-28 08:35:15 -0700

    updates

--- app/utils.js
+++ app/utils.js
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
 
 /**
  * @returns {string} A random string of characters
@@ -14,39 +14,39 @@ export const generateId = function() {
   return result;
 };
 
-export const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: "btn btn-success",
-    cancelButton: "btn btn-danger"
-  },
-  buttonsStyling: false
-});
+// export const swalWithBootstrapButtons = Swal.mixin({
+//   customClass: {
+//     confirmButton: "btn btn-success",
+//     cancelButton: "btn btn-danger"
+//   },
+//   buttonsStyling: false
+// });
 
-swalWithBootstrapButtons
-  .fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonText: "Yes, delete it!",
-    cancelButtonText: "No, cancel!",
-    reverseButtons: true
-  })
-  .then(result => {
-    if (result.value) {
-      swalWithBootstrapButtons.fire(
-        "Deleted!",
-        "Your file has been deleted.",
-        "success"
-      );
-    } else if (
-      /* Read more about handling dismissals below */
-      result.dismiss === Swal.DismissReason.cancel
-    ) {
-      swalWithBootstrapButtons.fire(
-        "Cancelled",
-        "Your imaginary file is safe :)",
-        "error"
-      );
-    }
-  });
+// swalWithBootstrapButtons
+//   .fire({
+//     title: "Are you sure?",
+//     text: "You won't be able to revert this!",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonText: "Yes, delete it!",
+//     cancelButtonText: "No, cancel!",
+//     reverseButtons: true
+//   })
+//   .then(result => {
+//     if (result.value) {
+//       swalWithBootstrapButtons.fire(
+//         "Deleted!",
+//         "Your file has been deleted.",
+//         "success"
+//       );
+//     } else if (
+//       /* Read more about handling dismissals below */
+//       result.dismiss === Swal.DismissReason.cancel
+//     ) {
+//       swalWithBootstrapButtons.fire(
+//         "Cancelled",
+//         "Your imaginary file is safe :)",
+//         "error"
+//       );
+//     }
+//   });
